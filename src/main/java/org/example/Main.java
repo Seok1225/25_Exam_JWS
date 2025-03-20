9376d0ac78d0d73c7eeb4d50fec089f922cf68ac
Seok1225 <koreait6642@gmail.com>
2025-03-20 16:15:00 +0900

    문제3번

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,17 @@
 package org.example;
-//1. 가로길이, 세로길이를 입력받아 사각형 넓이 만큼 별을 찍어내는 프로그램을 구현합니다(30점)
-//내용
-//- for문 혹은 while문 둘다 사용가능
-//
-//Scanner 활용
-//
-//
-//- 결과코드는 깃허브에 업로드
-
-import java.util.Scanner;
+//3. 프로그램은 정수 10개의 피보나치 수열 숫자를 출력합니다. 수열을 생성하기 위해 루프를 사용합니다. (40점)
+// for문 혹은 while문 둘다 사용가능
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        int width = Integer.parseInt(str.split(" ")[0]);
-        int height = Integer.parseInt(str.split(" ")[1]);
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " ");
+        for (int i = 0; i < 9; i++) {
+            System.out.printf(b + " ");
+            int c = a + b;
+            a = b;
+            b = c;
         }
     }
 }
